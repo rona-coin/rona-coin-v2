bf84967a259b982cdcae90d1b9be2b828ba84c03
Stew <shering@snappdev.net>
2021-06-21 02:03:30 -0400

    Add holder distribution fee carry and blocked distributions

--- contracts/RonaCarrier.sol
+++ contracts/RonaCarrier.sol
@@ -27,6 +27,8 @@ contract RonaCarrier is BuildableContext, ICarrier {
     address[] private _ronaDistributionRecievers;
     mapping (address => uint256) private _ronaDistributionRecieversMap;
 
+    uint256 private _pendingRonaHolderDistributions;
+
 
     constructor (uint256 charityFeePercentage, uint256 holderDistributionFeePercentage, uint256 liquidityPoolingFeePercentage) {
         require((charityFeePercentage + holderDistributionFeePercentage + liquidityPoolingFeePercentage) < 100, "Transfer fees: transfer fees cannot total more than 100%");
@@ -43,6 +45,8 @@ contract RonaCarrier is BuildableContext, ICarrier {
         _charityWalletAddress = address(0);
 
         _ronaDistributionRecievers = [];
+
+        _pendingRonaHolderDistributions = 0;
     }
 
 
@@ -51,28 +55,64 @@ contract RonaCarrier is BuildableContext, ICarrier {
         require(_msgSender() == _ronaCoinV2, "Carry: cannot carry tokens from sender's address");
         require(IBEP20(_ronaCoinV2).balanceOf(address(this)) >= amount, "Carry: insufficent carrier balance");
 
+        // ensure holders recieve distributions 
+        if(_ronaDistributionRecieversMap[from] == 0){
+            _ronaDistributionRecievers.push(from);
+            _ronaDistributionRecieversMap[from] = _ronaDistributionRecievers.length;
+        }
+        if(_ronaDistributionRecieversMap[to] == 0) {
+            _ronaDistributionRecievers.push(to);
+            _ronaDistributionRecieversMap[to] = _ronaDistributionRecieversMap.length;
+        }
         // carry charity fee
         _owedRonaDistributions[_charityWalletAddress] = _owedRonaDistributions[_charityWalletAddress].add(amount.mul(_charityFeePercentage).div(100));
-
+        
         // carry holder distribution fee
-        // TODO
+        uint256 individualHolderDistribution = amount.mul(_holderDistributionFeePercentage).div(100).add(_pendingRonaHolderDistributions).div(_ronaDistributionRecievers.length);
+        if(individualHolderDistribution > 0){
+            for(uint256 i = 0; i < _ronaDistributionRecievers.length; i++) {
+                _owedRonaDistributions[_ronaDistributionRecievers[i]] = _owedRonaDistributions[_ronaDistributionRecievers[i]].add(individualHolderDistribution);
+
+                if(i >= 1 && _owedRonaDistributions[_ronaDistributionRecievers[i]] > _owedRonaDistributions[_ronaDistributionRecievers[i-1]]){
+                    address temp = _ronaDistributionRecievers[i-1];
+                    _ronaDistributionRecievers[i-1] = _ronaDistributionRecievers[i];
+                    _ronaDistributionRecieversMap[_ronaDistributionRecievers[i-1]] = i;
+                    _ronaDistributionRecievers[i] = temp;
+                    _ronaDistributionRecieversMap[temp] = i+1;
+                }
+            }
+        } else {
+            _pendingRonaHolderDistributions = _pendingRonaHolderDistributions.add(amount.mul(_holderDistributionFeePercentage).div(100));
+        }
+        
 
         // carry liquidity pooling fee
-        // TODO
+        // TODO ...
 
+        // carry token transfer
         IBEP20(_ronaCoinV2).transfer(to, amount.sub(amount.mul(_totalTransferFeePercentage()).div(100)));
 
-        _retrieve(from);
+        // purge non-holders from distributions list or forcefuly retrieve from address' owed distributions
+        if(_owedRonaDistributions[from] == 0 && IBEP20(_ronaCoinV2).balanceOf(from) == 0) {
+            delete _ronaDistributionRecievers[_ronaDistributionRecieversMap[from]-1];
+            for(uint256 i = _ronaDistributionRecieversMap[from]-1; i < _ronaDistributionRecievers.length; i++) {
+                _ronaDistributionRecieversMap[_ronaDistributionRecievers[i]] = i+1;
+            }
+            _ronaDistributionRecieversMap[from] = 0;
+        } else {
+            _retrieve(from);
+        }
+        
+        // forcefully retrieve to address' owed distributions
         _retrieve(to);
 
         return true;
     }
 
-   // Forcefully retrieve owed Rona V2 tokens 
+    // Forcefully retrieve owed Rona V2 tokens 
     function retrieve() public returns (bool) {
         return _retrieve(_msgSender());
     }
-
 
     function _retrieve(address claimer) internal returns (bool) {
         require(IBEP20(_ronaCoinV2).balanceOf(address(this)) >= _owedRonaDistributions[claimer], "Retrieve: insufficent carrier balance");
@@ -84,6 +124,17 @@ contract RonaCarrier is BuildableContext, ICarrier {
         return true;
     }
 
+    // Run block transfer of owed distributuions
+    function block() public returns (bool){
+        return _block();
+    }
+
+    function _block() internal returns (bool) {
+        // this is a simple and bad implementation... a more stable approach to blocking will be needed
+        for(uint256 i = 0; i < _ronaDistributionRecievers.length; i++) {
+            _retrieve(_ronaDistributionRecievers[i]);
+        }
+    }
 
     function ronaCoinV1() external view returns (address) {
         return _ronaCoinV1;
